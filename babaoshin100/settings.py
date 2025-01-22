d6f0c5cd6086a99b367bcaf6bf464d09c20b8a6c
SamuelOshin <samuelt.oshin@gmail.com>
2025-01-22 23:21:47 +0100

    Edited the template

--- babaoshin100/settings.py
+++ babaoshin100/settings.py
@@ -84,6 +84,13 @@ WSGI_APPLICATION = 'babaoshin100.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': dj_database_url.config(
         default=env('DATABASE_URL')
